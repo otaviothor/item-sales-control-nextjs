f989c9007a58648938f86386b57a7028d5b0a13e
Otávio Silva <otaviosilva2632@gmail.com>
2025-04-15 10:28:14 -0300

    fix: build and return imports for observability

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { GeistSans } from "geist/font/sans";
 import type { Metadata } from "next";
 import { Navbar } from "./components/nav";
 import "./global.css";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const metadata: Metadata = {
   title: {
@@ -42,6 +44,9 @@ export default function RootLayout({
         <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
           <Navbar />
           {children}
+
+          <Analytics />
+          <SpeedInsights />
         </main>
       </body>
     </html>
